cf9769723ae5c9a1b2ffcb7addbdd7105182882b
Montiel257 <emimonty257@gmail.com>
2026-03-03 01:04:59 -0600

    Update Main.py

--- teoria/metodosVsMain/Main.py
+++ teoria/metodosVsMain/Main.py
@@ -4,13 +4,10 @@ from Menu import Menu
 class Main:
     pass
 
-# Ejecución del algoritmo principal
 if __name__ == "__main__":
-    # 1. Creamos los objetos
     mi_cuenta = Cuenta(3000, 5000)
     mi_menu = Menu()
 
-    # 2. Usamos los métodos
     mi_menu.darBienvenida()
     
     opcion = ""
