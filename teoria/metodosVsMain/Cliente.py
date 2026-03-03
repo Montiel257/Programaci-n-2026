93c437d81b52417476b3fc5fcd32020aad13ac85
Montiel257 <emimonty257@gmail.com>
2026-03-03 01:05:22 -0600

    Update Cliente.py

--- teoria/metodosVsMain/Cliente.py
+++ teoria/metodosVsMain/Cliente.py
@@ -1,12 +1,10 @@
 class Cliente:
     def __init__(self, nombre, direccion, edad):
-        # Guardamos los datos en los atributos del objeto
         self.nombre = nombre
         self.direccion = direccion
         self.edad = edad
 
     def imprimirDetalles(self):
-        # Este método solo imprime la información
         print("\n--- Detalles del Cliente ---")
         print(f"Nombre: {self.nombre}")
         print(f"Dirección: {self.direccion}")
