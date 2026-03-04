7d500c1be50efe69cccf3ab8c4e98cc0b549d5e9
Montiel257 <emimonty257@gmail.com>
2026-03-03 18:04:38 -0600

    Update Main.py

--- teoria/metodosVsMain/Main.py
+++ teoria/metodosVsMain/Main.py
@@ -1,19 +1,25 @@
+# Autor: Samuel Emiliano Perez Montiel
+# Descripción: Archivo principal que coordina la ejecución de las clases.
+
 from Cuenta import Cuenta
 from Menu import Menu
 
 class Main:
+    """Clase principal vacía según requerimiento"""
     pass
 
 if __name__ == "__main__":
-    mi_cuenta = Cuenta(3000, 5000)
-    mi_menu = Menu()
-
-    mi_menu.darBienvenida()
+    # 1. Crear el objeto Cuenta (los datos de la cuenta)
+    mi_cuenta = Cuenta(3000, "Ahorro", "01/01/2023")
     
-    opcion = ""
-    while opcion != "4":
-        opcion = mi_menu.despliegaMenu()
-        if opcion != "4":
-            mi_menu.procesaOpcion(opcion, mi_cuenta)
-            
-    print("Gracias por usar el sistema.")
+    # 2. Crear el objeto Menu
+    mi_menu = Menu("BIENVENIDO A BANCOS MONTILIANOS")
+    
+    # 3. Ejecutar flujo
+    mi_menu.dar_bienvenida()
+    
+    # El ciclo permite que el usuario haga varias cosas sin que el programa se cierre
+    opcion_elegida = ""
+    while opcion_elegida != "4":
+        opcion_elegida = mi_menu.despliegaMenu()
+        mi_menu.procesaOpcion(opcion_elegida, mi_cuenta)
