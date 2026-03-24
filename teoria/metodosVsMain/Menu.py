03d5904b1f7a9b5a734e86b402a2534a092920c6
Montiel257 <emimonty257@gmail.com>
2026-03-24 05:42:26 -0600

    Update Menu.py

--- teoria/metodosVsMain/Menu.py
+++ teoria/metodosVsMain/Menu.py
@@ -1,30 +1,21 @@
+"""
+Created on Marzo, 2026
+@author: Montiel257
+"""
+
+from Cuenta import *
+
 class Menu:
-    def __init__(self, mensajeDeBienvenida):
-        self.mensajeDeBienvenida = mensajeDeBienvenida
 
-    def darBienvenida(self):
-        print(self.mensajeDeBienvenida)
+    def __init__(self, mensaje):
+        self.mensaje = mensaje
 
-    def despliegaMenu(self):
-        print("--- Menu del Banco ---")
-        print("1. Consultar saldo")
-        print("2. Depositar")
-        print("3. Retirar")
+    def bienvenida(self):
+        print(self.mensaje)
+
+    def opciones(self):
+        print("\n1. Depositar")
+        print("2. Retirar")
+        print("3. Mostrar cuenta")
         print("4. Salir")
-        opcion = input("Que numero de opcion quieres? ")
-        return opcion
-
-    def procesaOpcion(self, opcion, cuenta):
-        if opcion == '1':
-            print("Titular:", cuenta.titular)
-            print("Saldo:", cuenta.cantidad)
-        elif opcion == '2':
-            monto = float(input("Cuanto vas a depositar? "))
-            cuenta.depositar(monto)
-        elif opcion == '3':
-            monto = float(input("Cuanto vas a retirar? "))
-            cuenta.retirar(monto)
-        elif opcion == '4':
-            print("Adios")
-        else:
-            print("Opcion incorrecta")
+        return input("Opcion: ")
