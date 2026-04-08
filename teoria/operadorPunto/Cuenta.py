57966201b2993b509d6d0df1a96e3ae86f1a4319
Montiel257 <emimonty257@gmail.com>
2026-04-07 19:10:13 -0600

    Update Cuenta.py

--- teoria/operadorPunto/Cuenta.py
+++ teoria/operadorPunto/Cuenta.py
@@ -5,7 +5,24 @@ Created on Febrero,2026
 '''
 class Cuenta:
 
-	def __init__(self, valor, tipo, titular):
-		self.cantidad = valor
-		self.tipo = tipo
-		self.titular = titular
+    # Método constructor
+    # Inicializa los atributos de la cuenta
+    def __init__(self, saldo, tipo, fechaCreacion):
+        self.saldo = saldo
+        self.tipo = tipo
+        self.fechaCreacion = fechaCreacion
+
+    # Método para depositar dinero
+    def depositar(self, cantidad):
+        self.saldo = self.saldo + cantidad
+
+    # Método para retirar dinero
+    def retirar(self, cantidad):
+        self.saldo = self.saldo - cantidad
+
+    # Método para mostrar la información de la cuenta
+    def imprimirDetalles(self):
+        print("saldo:", self.saldo)
+        print("tipo:", self.tipo)
+
+        print("fechaCreacion:", self.fechaCreacion)
