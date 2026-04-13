1a9b3e88e499ab02d92373a2adff5b43cd0c3559
Montiel257 <emimonty257@gmail.com>
2026-04-12 19:32:13 -0600

    Create Cliente.py

---
+++ banco/Cliente.py
@@ -0,0 +1,10 @@
+class Cliente_inversion:
+
+    def __init__(self, nombre, empresa):
+        self.nombre = nombre
+        self.empresa= nombre de la empresa 
+
+    def __str__(self):
+        cadena = "\nNombre del cliente: " + self.nombre
+        cadena += "\nempresa: " + str(self.empresa)
+        return cadena
