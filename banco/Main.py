d311ce1c008499235a7c4cd6b56763e1833763c5
Montiel257 <emimonty257@gmail.com>
2026-04-12 19:33:49 -0600

    Create Main.py

---
+++ banco/Main.py
@@ -0,0 +1,37 @@
+from Inversion import *
+from Cliente_inversion import *
+from Menu import *
+
+menu = Menu("BIENVENIDO")
+
+inversion1 = Inversion(3000, 4.5, 24, "NVIDIA")
+
+menu.bienvenida()
+
+while True:
+
+    opcion = menu.opciones()
+
+    if opcion == "1":
+        cantidad = float(input("Cantidad a depositar: "))
+        inversion1.depositar(cantidad)
+        print("Capital actualizado:", inversion1.capital)
+
+    elif opcion == "2":
+        cantidad = float(input("Cantidad a retirar: "))
+        inversion1.retirar(cantidad)
+        print("Capital actualizado:", inversion1.capital)
+
+    elif opcion == "3":
+        resultado = inversion1.calcular()
+        print("Monto final de la inversion:", resultado)
+
+    elif opcion == "4":
+        inversion1.mostrar()
+
+    elif opcion == "5":
+        print("Saliendo del sistema...")
+        break
+
+    else:
+        print("Opcion no valida")
